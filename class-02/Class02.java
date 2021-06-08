8affbc198922cb1a15c28dfbc7521ef90631ba3a
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2021-06-09 01:35:31 +0500

    Naming convention, Method & Functions, Arthimatic Op, Short Hand Op, Conditions

---
+++ class-02/Class02.java
@@ -0,0 +1,88 @@
+package pkgclass.pkg02;
+public class Class02 {
+    public static void main(String[] args) {
+        /* Topics:
+        
+          =>Naming convention:
+            1. We can use $ & _ in our variable.
+            2. Variable name should be in Camel Case.
+            3. Class name first letter should be capital.
+            4. Class name should be Noun (Student, Person)
+        
+          => Method & Functions
+            A method, like a function, is a set of instructions that perform a task. 
+            The difference is that a method is associated with an object, while a function is not.
+        
+          => Arthimatic Operations (+,-,*,/)
+          => Short Hand Operators
+          => Pre, Post Increment and Decrement
+          => Conditions (IF & Else)
+        */
+
+//        Naming Convention Example Start
+
+//        String myName = "Muhammad Ashar";
+//        System.out.println(myName);
+
+//        Naming Convention Example End
+
+//        Arthimatic Operations Example Start
+
+//          int numOne = 10;
+//          int numTwo = 20;
+//          int numThree = 10;     
+//          int addtion = numOne + numTwo + numThree;
+//          int subtraction = numTwo - numThree;
+//          int multiplication = numTwo * numOne;
+//          double division = numOne / numTwo;
+//          
+//          System.out.println(addtion);
+//          System.out.println(subtraction);
+//          System.out.println(multiplication);
+//          System.out.println(division);
+
+//        Arthimatic Operations Example End
+
+//            String myName = "Shakir";
+//            myName = "Ashar";
+//            System.out.println(myName);
+
+//          Short Hand Operators Example Start
+
+//            int numOne = 1;
+//            int numTwo = 2;
+////            numOne = numOne + numTwo; 
+//            numOne += numTwo; 
+//            System.out.println(numOne);
+
+//          Post Incrment & Pre Increments
+//            int valueOne = 10;
+//            int valueTwo = 20;
+//            int valueThree = ++valueOne;
+//            int valueFour = valueThree++;
+//            int valueFive = valueFour;
+//            Expected Ans : 12
+
+//          Post DeIncrment & Pre DeIncrment
+//            int subOne = 10;
+//            int subAns = --subOne;
+//            
+//            System.out.println(subAns);
+
+//          Short Hand Operators Example End
+
+//        Conditions Example Start
+//          int userAge = 17;
+//          if(userAge >= 18)
+//          {
+//              System.out.println("Welcome to our application");
+//          }
+//          else
+//          {
+//              System.out.println("Your are under age");
+//          }
+
+//        Conditions Example End
+          
+        }  
+    }
