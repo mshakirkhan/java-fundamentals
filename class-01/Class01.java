5013d47a4dfff6fd90371bc75ca3e6aec6f6700d
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2021-06-09 01:20:13 +0500

    Java Common Data Types

---
+++ class-01/Class01.java
@@ -0,0 +1,18 @@
+package pkgclass.pkg01;
+public class Class01 {
+    public static void main(String[] args) {
+        //  Java Common Data Types
+
+        int myNum = 5;               // integer (whole number)
+        float myFloatNum = 5.99f;    // floating point number
+        char myLetter = 'D';         // character
+        boolean myBool = true;       // boolean
+        String myText = "Hello";     // String   
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    } 
+}
