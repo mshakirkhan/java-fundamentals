fc3e6bd6fd03b5c098226137a44c8491eca79186
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2021-07-01 01:21:31 +0500

    Class 03

---
+++ class-03/Class03.java
@@ -0,0 +1,146 @@
+package pkgclass.pkg03;
+public class Class03 {
+
+    public static void main(String[] args) {   
+        /* Topics:
+        
+        => Modulus operator
+        => Nested Conditions
+        => Turnary Operator
+        => Switch
+        => Code Redundancy
+        => Infinite loop
+        => While loop
+        => Do while loop
+        => For loop
+        => Table of 5
+        
+        */
+        
+//        Modulus operator start
+
+//        int valueOne = 2;
+//        int valueTwo = 21;
+//        int result = valueTwo % valueOne;
+//        System.out.println(result);
+
+//        Modulus operator end
+
+//        Example No is even or odd start
+//        int num = 11;
+//        if(num%2 == 0)
+//        {
+//            System.out.println("This value is Even");
+//        }
+//        else
+//        {
+//            System.out.println("This value is Odd");
+//        }
+//        Example No is even or odd start
+
+//        Nested IF Start
+
+//          int age = 19;
+//          if(age == 18)
+//          {
+//              System.out.println("You are 18");
+//          }
+//          else if(age == 19)
+//          {
+//              System.out.println("You are 19");
+//          }
+//          else if(age == 20)
+//          {
+//              System.out.println("You are 20");
+//          }
+//          else
+//          {
+//              System.out.println("No Match");
+//          }
+
+//        Nested IF Start
+
+//          Turnary Operator Start
+
+//            int age = 17;
+//            String result = (age>= 18) ? "Yes are 18" : "You are under age";
+//            System.out.println(result);
+
+//          Turnary Operator Close
+
+//         Switch Case Start
+
+//            int a = 1;
+//            switch(a)
+//            {
+//                case 1:
+//                    System.out.println("This is 1");
+//                    break;
+//                case 2:
+//                     System.out.println("This is 2");
+//                     break;
+//                case 3:
+//                    System.out.println("This is 3");
+//                    break;
+//                default:
+//                    System.out.println("No Match");
+//            }
+
+//          Switch Case Start
+
+//          Code Redundancy Start
+
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+//            System.out.println("Value of 10");
+
+//          Code Redundancy Close
+
+//          While Loop Example Start
+//              int i = 1;
+//              while(i <= 10) //
+//              {
+//                  System.out.println("Value of 10");
+//                  i++;
+//              }
+
+//          While Loop Example End
+
+//          DO While Loop Example Start
+
+//              Do while loop
+//                int i =20;
+//                do
+//                {
+//                    System.out.println("Value of 10");
+//                    i++;
+//                }while(i <= 10);
+              
+//          Do While Loop Example Close
+
+//          For Loop Example Start
+
+//            for(int i =1; i<=10; i++)
+//            {  
+//                System.out.println("Value of 10");
+//            }
+
+//          For Loop Example Close
+//            
+//          Example Table of 10 Start using for loop start
+//              int num = 10;
+//              for(int i = 1; i<=10; i++)
+//              {
+//                  System.out.println(num + "x" + i + "=" + num*i);
+//              }
+//          Example Table of 10 Start using for loop End                 
+    }
+    
+}
